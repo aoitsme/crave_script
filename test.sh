4ecf207a6a96a328602f841516681ee795887e2a
aoitsme <kanonify01@gmail.com>
2026-05-15 13:33:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -16,7 +16,7 @@ repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --n
 repo sync
 
 # Clone device tree
-git clone https://github.com/aoitsme/stardust_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
+git clone https://github.com/aoitsme/android_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
 git clone https://github.com/aoitsme/android_device_sony_aurora -b lineage-23.2 device/sony/aurora
 git clone https://github.com/aoitsme/android_device_sony_tama-common -b crdroid-16.0 device/sony/tama-common
 git clone https://github.com/aoitsme/android_hardware_sony_SonyOpenTelephony -b lineage-23.2 hardware/sony/SonyOpenTelephony
