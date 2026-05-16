295f192632990c92d1147c5509c3964a63e30eea
aoitsme <kanonify01@gmail.com>
2026-05-16 16:34:08 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 # WARNING: This will remove all local changes!
 rm -rf .repo/local_manifests
-rm -rf bionic
 rm -rf kernel/sony
 rm -rf device/sony
 rm -rf hardware/sony
@@ -10,16 +9,16 @@ rm -rf vendor/sony
 rm -rf vendor/lineage-priv
 
 # Initialize repo
-repo init -u https://github.com/crdroidandroid/android.git -b 16.0 --git-lfs --no-clone-bundle
+repo init -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 
 # Sync the repositories
 /opt/crave/resync.sh
-repo sync
+repo sync -c --force-sync --no-clone-bundle --no-tags
 
 # Clone device tree
 git clone https://github.com/aoitsme/android_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
-git clone https://github.com/aoitsme/android_device_sony_aurora -b lineage-23.2 device/sony/aurora
-git clone https://github.com/aoitsme/android_device_sony_tama-common -b crdroid-16.0 device/sony/tama-common
+git clone https://github.com/aoitsme/android_device_sony_aurora -b lunaris-16.2 device/sony/aurora
+git clone https://github.com/aoitsme/android_device_sony_tama-common -b lineage-23.2 device/sony/tama-common
 git clone https://github.com/aoitsme/android_hardware_sony_SonyOpenTelephony -b lineage-23.2 hardware/sony/SonyOpenTelephony
 git clone https://github.com/aoitsme/proprietary_vendor_sony_aurora -b lineage-23.2 vendor/sony/aurora
 git clone https://github.com/aoitsme/proprietary_vendor_sony_tama-common -b lineage-23.2 vendor/sony/tama-common
@@ -29,5 +28,5 @@ git clone https://github.com/aoitsme/keys -b master vendor/lineage-priv
 . build/envsetup.sh
 
 # Build rom
-rm -rf out
-brunch aurora
+lunch lineage_aurora-bp4a-user
+m bacon
