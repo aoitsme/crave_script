0a58ddfbbe92a0cc4458680d2e284a4f8aa506f5
aoitsme <kanonify01@gmail.com>
2026-08-17 15:43:08 +0700

    Update lineage.sh

--- lineage.sh
+++ lineage.sh
@@ -154,7 +154,7 @@ start_build_process() {
     echo "Cloning device trees..."
     git clone https://github.com/aoitsme/android_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
     git clone https://github.com/aoitsme/android_device_sony_"$DEVICE_CODE" -b lineage-23.2 device/sony/"$DEVICE_CODE"
-    git clone https://github.com/aoitsme/android_device_sony_tama-common -b lineage-23.2-prvt device/sony/tama-common
+    git clone https://github.com/aoitsme/android_device_sony_tama-common -b derp-16.2 device/sony/tama-common
     git clone https://github.com/aoitsme/android_hardware_sony_SonyOpenTelephony -b lineage-23.2 hardware/sony/SonyOpenTelephony
     git clone https://github.com/aoitsme/proprietary_vendor_sony_"$DEVICE_CODE" -b lineage-23.2 vendor/sony/"$DEVICE_CODE"
     git clone https://github.com/aoitsme/proprietary_vendor_sony_tama-common -b lineage-23.2 vendor/sony/tama-common
@@ -162,6 +162,7 @@ start_build_process() {
     
     echo "Starting ROM build..."
     . build/envsetup.sh
+    export PRODUCT_NAME=derpfest_akari
     lunch lineage_"$DEVICE_CODE"-bp4a-userdebug
     mka derp
 
