62551d5de0dae8acc27b64d95526fe76b44576a5
aoitsme <kanonify01@gmail.com>
2026-08-30 08:20:46 +0700

    Update derp.sh

--- derp.sh
+++ derp.sh
@@ -135,7 +135,7 @@ start_build_process() {
     if [ -f /opt/crave/resync.sh ]; then
       /opt/crave/resync.sh
     else
-      repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune
+      repo sync
     fi
     
     echo "Replacing some repository..."
